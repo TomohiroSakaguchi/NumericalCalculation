da35d7d6cd0c29eeb7438737bfbe2fa8896b70ce
TomohiroSakaguchi <stomohiro59@gmail.com>
2021-08-23 16:20:36 +0900

    たぶんこれが正解

--- a3-5419048.c
+++ a3-5419048.c
@@ -3,13 +3,14 @@ int main(void)
 	{
 		int a,s,i;
 		s=0;
-		scanf("%d\n",&a);
-		for(i=2;i<=a;i++){
+		scanf("%d",&a);
+		printf("%dの約数は", a);
+		for(i=1;i<=a;i++){
 			s=a%i;
 			if(s==0){
-				printf("%dの約数は%d\n",a,s);
+				printf("%d ",i);
 			}
 		}
-
+		printf("\n");
 		return 0;
 	}

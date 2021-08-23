c93b9f93634d26ac5a6b7bf59476a7fbebea8abf
TomohiroSakaguchi <stomohiro59@gmail.com>
2021-08-23 16:05:42 +0900

    2次方程式

---
+++ 5419048.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(void){
+	int a,b,c;
+	double x1,x2;
+	a=1.0;
+	b=3.0;
+	c=2.0;
+x1=(-b+sqrt((b^2)-(4*a*c)))/(2*a);
+x2=(-b-sqrt((b^2)-(4*a*c)))/(2*a);
+
+printf("x1=%lf\n",x1);
+printf("x2=%lf\n",x2);
+return 0;
+}

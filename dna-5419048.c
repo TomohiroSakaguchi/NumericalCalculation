0db0832437892f769c3a84b4cb2ca1ccf145578c
TomohiroSakaguchi <stomohiro59@gmail.com>
2021-08-25 21:18:51 +0900

    負の解求まったぞ〜

--- dna-5419048.c
+++ dna-5419048.c
@@ -1,30 +1,33 @@
 #include<stdio.h>
+#include <math.h>
 double f(double x);
 int main (void){
 	int i;
 	double a,b,m,za,zb,zm,p,q,n,zp,zq,zn;
+	double significant = 3;//小数点以下どこまで使うのか
+	double threshold;
 	a=-4.0;
 	b=-1.0;
 	p=0.4;
 	q=3.0;
 	//負の解を求める
+	int point = round(pow(10,significant));
 	for(i=0;i<50;i++){
 		m=(a+b)/2.0;
 		za=f(a);
 		zb=f(b);
-		printf("負の解m=%lf,f=%lf, iteration=%d\n",m,zm, i+1);
-		if(f(m) < 0.0){
+		threshold=round(f(m)*point)/point;
+		printf("負の解m=%lf,threshold=%lf, iteration=%d\n",m,threshold, i+1);
+		if(threshold > 0.0){
 			a=m;
-		}else if (f(m) == 0) {
+		}else if (threshold == 0) {
+			printf("break!\n");//for抜け確認用
 			break;
 		} else {
 			b=m;
 		}
-		//if(zm<(-1/100000)){
-		//break;
-		//}
 	}
-	printf("正の解");
+	//正の解を求める;
 	//for
 	return 0;
 }

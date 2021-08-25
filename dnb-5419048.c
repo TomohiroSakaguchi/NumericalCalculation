82a265c2266280cec7073f2907872aedfeaa2c69
TomohiroSakaguchi <stomohiro59@gmail.com>
2021-08-25 22:11:33 +0900

    ニュートン法うまく行った

--- dnb-5419048.c
+++ dnb-5419048.c
@@ -2,20 +2,31 @@
 #include <math.h>
 double f(double x);
 double df(double x);
+double dfm(double x);
 int main(void){
 	int i;
-	double a,z,eps;
-	a=1.0;
-	eps = 1.0e-2;
+	double x,eps;
+	x=5.0;
+	eps = 1.0e-4;
 	for(i=0;i<=1000;i++){
-		z=a-f(a)/df(a);
-
-		if(fabs(z-a)<eps){
+		double x1 = x - f(x)/df(x);
+		if(fabs(x-x1)<eps){
 			break;
 		}
-		a=z;
-		printf("a=%lf,z-a=%.8f\n",a,z-a);
+		x=x1;
+		printf("x1=%lf,x-x1=%.8f\n",x,x-x1);
 	}
+	x = -10.0;
+	for(i=0;i<=1000;i++){
+		double x1 = x - f(x)/df(x);
+		//printf("%lf\n", x1);
+		if(fabs(x1-x)<eps){
+			break;
+		}
+		x=x1;
+		printf("x2=%lf,x-x1=%.8f\n",x,x-x1);
+	}
+
 	return 0;
 }
 double f(double x){
@@ -26,5 +37,11 @@ double f(double x){
 double df(double x){
 	double d;
 	d=2.0*x;
+	return d;
+}
+
+double dfm(double x){
+	double d;
+	d = -2.0*x;
 	return d;
 }

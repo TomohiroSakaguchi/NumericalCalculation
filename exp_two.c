5be917da42fee15ee32a569753b6b11d61d3baf4
TomohiroSakaguchi <stomohiro59@gmail.com>
2021-08-26 17:58:24 +0900

    収束した

--- exp_two.c
+++ exp_two.c
@@ -6,10 +6,10 @@ int main (void){
 	double a,b,m,za,zb,zm,p,q,n,zp,zq,zn;
 	double significant = 7;//小数点以下どこまで使うのか
 	double threshold;
-	a=-4.0;
-	b=-0.1;
-	p=-1.0;
-	q=3.0;
+	a=-1.0;
+	b=3.0;
+	p=-4.0;
+	q=-0.01;
 	//単調減少する方の解を求める
 	int point = round(pow(10,significant));
 	for(i=0;i<50;i++){
@@ -17,7 +17,7 @@ int main (void){
 		za=f(a);
 		zb=f(b);
 		threshold=round(f(m)*point)/point;
-		printf("負の解m=%lf,threshold=%lf, iteration=%d\n",m,threshold, i+1);
+		printf("単調減少の解m=%lf,threshold=%lf, iteration=%d\n",m,threshold, i+1);
 		if(threshold > 0.0){
 			a=m;
 		}else if (threshold == 0) {
@@ -34,7 +34,7 @@ int main (void){
 		zp=f(p);
 		zq=f(q);
 		threshold=round(f(zm)*point)/point;
-		printf("正の解m=%lf,threshold=%lf, iteration=%d\n",zm,threshold, i+1);
+		printf("単調増加の解m=%lf,threshold=%lf, iteration=%d\n",zm,threshold, i+1);
 		if(threshold < 0.0){
 			p=zm;
 		}else if (threshold == 0) {
@@ -48,7 +48,7 @@ int main (void){
 }
 
 double f(double x){
-	double c,e=94.8;
+	double c,e=104.8;
 	c=2*exp(-e*pow(x,2))-exp(e*x);
 	//printf("f(x) = %lf\n", c);
 	return c;
